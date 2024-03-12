3ec928449e6ae95928e28903e7b7a30735389ebc
BobdaFett <lvas463@gmail.com>
2024-03-12 14:37:12 -0400

    Added files that were deleted by accident.

---
+++ InfiniteFlashlight/Patches/FlashlightItemPatch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace InfiniteFlashlight.Patches
+{
+    [HarmonyPatch(typeof(FlashlightItem))]
+    internal class FlashlightItemPatch
+    {
+        [HarmonyPatch(nameof(FlashlightItem.Update))]
+        [HarmonyPostfix]
+        static void FlashlightItemUpdatePostfix(FlashlightItem instance)
+        {
+            if (instance.IsOwner)
+            {
+                if (instance.isBeingUsed && instance.itemProperties.requiresBattery)
+                {
+                    if ((double)instance.insertedBattery.charge > 0.0)
+                    {
+                        if (instance.itemProperties.itemIsTrigger)
+                            instance.insertedBattery.charge += Time.deltaTime / instance.itemProperties.batteryUsage;
+                    }
+                }
+            }
+        }
+    }
+}
